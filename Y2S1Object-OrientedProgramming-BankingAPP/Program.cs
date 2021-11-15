37e0cd4165b7a5fdc67b0dccc6a3e6a06eb7a624
Itamar Junior <itjunior@gmail.com>
2021-11-15 23:38:37 +0000

    First Commit

---
+++ Y2S1Object-OrientedProgramming-BankingAPP/Program.cs
@@ -0,0 +1,20 @@
+﻿//Student ID 23952 Itamar Junior
+using System;
+
+namespace Y2S1ObjectOrientedProgrammingBankingAPP
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Banking TriploXXX System");
+
+            Console.WriteLine("Are you a: \n1-Employee or an \n2-Customer? \nType your option:");
+
+            int userOption = Convert.ToInt32(Console.ReadLine());
+
+            models.Login.validatorSystem(userOption);
+
+        }
+    }
+}
